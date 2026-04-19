d2f35e525358d68c46accf7d5296559f3c236952
Himanshu Yadav <codeturbulent@gmail.com>
2026-04-19 06:10:11 +0530

    added git ignore

---
+++ myapp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello people");
+
+// Console.ReadLine();
+/*dfsdfs dfsd fsdfsdf sdf df d */
---
+++ newp.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the classic structure!");
+            Console.ReadLine();
+
+    }
+    }}
